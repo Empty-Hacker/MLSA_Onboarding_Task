8121f22e1ba8b63cc67b469dc536fd874842898d
Empty-Hacker <120492625+Empty-Hacker@users.noreply.github.com>
2023-12-14 15:10:47 +0530

    changes_1

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Herish Chaniyara",
+  title: "Ethical Hacker and Web Developer",
+  email: "Herish.Chaniyara@studentambassadors.com",
+  gitHub: "Empty-Hacker",
+  instagram: "",
+  linkedIn: "Herish09",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/mlsadp.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Here is the dp!!!";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Amazon Clone Website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is the Clone Website of Amazon.com Website.",
+    url: "https://empty-hacker.github.io/Amazon-Clone/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Netflix Clone Website",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Here is a Clone Website of Netflix.",
+    url: "https://empty-hacker.github.io/Netflix-Clone/",
   },
   {
-    title: "My Resume Site",
+    title: "Femprish Website",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Femprish Website that I developed for the a NGO.",
+    url: "https://empty-hacker.github.io/Femprish/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "GitHub MLSA Task Repo",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "All details related to MLSA technical onboarding is here.",
+    url: "https://github.com/Empty-Hacker/MLSA_Onboarding_Task",
   },
 ];
 
